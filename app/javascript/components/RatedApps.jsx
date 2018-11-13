5d26bf088e2b65989954a58e0e860dd7152547b2
Alan Brennan <alan.brennan@shopify.com>
2018-11-13 16:13:53 +0000

    added-Apollo

--- app/javascript/components/RatedApps.jsx
+++ app/javascript/components/RatedApps.jsx
@@ -2,11 +2,38 @@ import React from 'react';
 import AppDetails from './AppDetails';
 import ExtraDetails from './ExtraDetails';
 import OneAppDetails from './OneAppDetails';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 export default class RatedApps extends React.Component {
 
 
   render(){
+
+    const httpLink = createHttpLink({uri: "http://localhost:3000/graphql"});
+    const client = new ApolloClient({
+      link: httpLink,
+      cache: new InMemoryCache()
+    });
+
+    const ALL_APPS_QUERY = gql`
+      query ALL_APPS_QUERY {
+        app {
+            id
+            name
+            rating
+        }
+      }`;
+
+    client.query({query: ALL_APPS_QUERY
+})
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
 
     var header = this.props.header.toString()
 
